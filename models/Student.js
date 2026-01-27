cf4a6a1ee5b2e0708a316498508f3ecf1b9b49d4
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-01-27 21:23:56 +0530

    initial commit

---
+++ models/Student.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  department: String,
+  email: String
+});
+
+module.exports = mongoose.model("Student", studentSchema);
---
+++ routes/studentRoutes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const Student = require("../models/Student");
+
+const router = express.Router();
+
+
+// CREATE student
+router.post("/add", async (req, res) => {
+  const student = new Student(req.body);
+  await student.save();
+  res.send("Student added");
+});
+
+
+// READ students
+router.get("/", async (req, res) => {
+  const students = await Student.find();
+  res.json(students);
+});
+
+
+// UPDATE student
+router.put("/update/:id", async (req, res) => {
+  await Student.findByIdAndUpdate(req.params.id, req.body);
+  res.send("Student updated");
+});
+
+
+// DELETE student
+router.delete("/delete/:id", async (req, res) => {
+  await Student.findByIdAndDelete(req.params.id);
+  res.send("Student deleted");
+});
+
+module.exports = router;
